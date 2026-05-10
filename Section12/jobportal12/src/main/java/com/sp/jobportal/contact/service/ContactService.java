52f8bd3d5e970d0bb596deeb351b11b484d465b0
surajvns1571999 <surajvns1571999@gmail.com>
2026-05-11 05:16:12 +0530

    Create jobportal12 section

---
+++ Section12/jobportal12/src/main/java/com/sp/jobportal/contact/service/ContactService.java
@@ -0,0 +1,9 @@
+package com.sp.jobportal.contact.service;
+
+import com.sp.jobportal.dto.ContactRequestDto;
+
+public interface ContactService {
+
+	public boolean saveContact(ContactRequestDto contactRequestDto);
+
+}
