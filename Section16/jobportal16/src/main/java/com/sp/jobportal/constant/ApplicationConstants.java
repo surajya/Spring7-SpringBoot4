81874e503ccf6ba06846cba627c02de0ab564e8b
surajvns1571999 <surajvns1571999@gmail.com>
2026-06-23 06:48:57 +0530

    Fix the admin login url

--- Section16/jobportal16/src/main/java/com/sp/jobportal/constant/ApplicationConstants.java
+++ Section16/jobportal16/src/main/java/com/sp/jobportal/constant/ApplicationConstants.java
@@ -6,6 +6,8 @@ public class ApplicationConstants {
     public static final String JWT_SECRET_DEFAULT_VALUE = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
     public static final String JWT_HEADER = "Authorization";
     public static final String ROLE_JOB_SEEKER = "ROLE_JOB_SEEKER";
+    public static final String ROLE_EMPLOYER = "ROLE_EMPLOYER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String JOB_STATUS = "ACTIVE";
     public static final String NEW_MESSAGE = "NEW";
     public static final String CLOSED_MESSAGE = "CLOSED";
