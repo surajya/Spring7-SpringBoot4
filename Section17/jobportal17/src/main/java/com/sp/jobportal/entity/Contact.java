f0dc35d346750ccc08ef1b56f5138a91af522103
surajvns1571999 <surajvns1571999@gmail.com>
2026-07-02 07:01:20 +0530

    Roll back operation performed successfully

--- Section17/jobportal17/src/main/java/com/sp/jobportal/entity/Contact.java
+++ Section17/jobportal17/src/main/java/com/sp/jobportal/entity/Contact.java
@@ -1,46 +1,43 @@
 package com.sp.jobportal.entity;
 
-import org.hibernate.annotations.ColumnDefault;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "contacts")
+@NamedQueries({
+        @NamedQuery(name = "Contact.updateStatusById",
+                query = "Update Contact c Set c.status = :status, c.updatedAt = current timestamp, c.updatedBy = :updatedBy where c.id = :id")
+})
 public class Contact extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-	@Column(name = "email", nullable = false)
-	private String email;
+    @Column(name = "email", nullable = false)
+    private String email;
 
-	@Lob
-	@Column(name = "message", nullable = false)
-	private String message;
+    @Lob
+    @Column(name = "message", nullable = false)
+    private String message;
 
-	@Column(name = "name", nullable = false)
-	private String name;
+    @Column(name = "name", nullable = false)
+    private String name;
 
-	@ColumnDefault("'NEW'")
-	@Column(name = "status", nullable = false, length = 20)
-	private String status;
+    @ColumnDefault("'NEW'")
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
 
-	@Column(name = "subject", nullable = false)
-	private String subject;
+    @Column(name = "subject", nullable = false)
+    private String subject;
 
-	@Column(name = "user_type", nullable = false, length = 50)
-	private String userType;
+    @Column(name = "user_type", nullable = false, length = 50)
+    private String userType;
 
 }
